e8c84a7eee1bb25deed87d121103381e036f00fd
ChuRit <churit@naver.com>
2025-02-14 13:37:44 +0900

    퍼즐 수정

--- puzzles.js
+++ puzzles.js
@@ -1,6 +1,6 @@
 // 퍼즐 맵 데이터
 const PUZZLE_MAPS = [
-    {
+    {   // 1
         id: 1,
         name: "Tutorial 1",
         size: 5,
@@ -22,7 +22,7 @@ const PUZZLE_MAPS = [
             [2, 2, 2, 2, 2]
         ]
     },
-    {
+    {   // 2
         id: 2,
         name: "Tutorial 2",
         size: 5,
@@ -44,7 +44,7 @@ const PUZZLE_MAPS = [
             [2, 2, 2, 2, 2]
         ]
     },
-    {
+    {   // 3
         id: 3,
         name: "Tutorial 3",
         size: 4,
@@ -73,7 +73,7 @@ const PUZZLE_MAPS = [
             [0, 0, 0, 0]
         ]
     },
-    {
+    {    // 4
         id: 4,
         name: "Tutorial 4",
         size: 4,
@@ -106,9 +106,38 @@ const PUZZLE_MAPS = [
             [2, 1, 0, 1]
         ]
     },
-    {
+    {   // 5
         id: 5,
         name: "Tutorial 5",
+        size: 4,
+        areas: [
+            {
+                cells: [[0,0], [0,1]],
+                required: 1
+            },
+            {
+                cells: [[3,2], [3,3]],
+                required: 1
+            },
+            {
+                cells: [[2,0], [3,0], [2,1], [2,2]],
+                required: 4
+            },
+            {
+                cells: [[0,3], [1,3], [1,2], [1,1]],
+                required: 4
+            }
+        ],
+        initialState: [
+            [0, 0, 0, 0],
+            [2, 0, 0, 0],
+            [0, 0, 0, 2],
+            [0, 0, 0, 0]
+        ]
+    },
+    {   // 6
+        id: 6,
+        name: "level 6",
         size: 4,
         areas: [
             {   // A
@@ -152,9 +181,9 @@ const PUZZLE_MAPS = [
             [0, 0, 0, 0]
         ]
     },
-    {
-        id: 6,
-        name: "Tutorial 6",
+    {   // 7
+        id: 7,
+        name: "level 7",
         size: 5,
         areas: [
             {
@@ -162,16 +191,20 @@ const PUZZLE_MAPS = [
                 required: 1
             },
             {
-                cells: [[1,1], [1,2]],
-                required: 1
+                cells: [[1,0], [1,1], [1,2]],
+                required: 2
             },
             {
-                cells: [[2,2], [2,3]],
-                required: 1
+                cells: [[2,2], [2,3],[2,4]],
+                required: 2
             },
             {
                 cells: [[3,3], [3,4]],
                 required: 1
+            },
+            {
+                cells: [[0,4]],
+                required: 0
             },
             {
                 cells: [[4,0], [4,1], [4,2], [4,3], [4,4]],
@@ -186,9 +219,9 @@ const PUZZLE_MAPS = [
             [0, 0, 0, 0, 0]
         ]
     },
-    {
-        id: 7,
-        name: "Tutorial 7",
+    {   // 8
+        id: 8,
+        name: "level 8",
         size: 6,
         areas: [
             {
@@ -221,9 +254,9 @@ const PUZZLE_MAPS = [
             [0, 0, 0, 0, 0, 0]
         ]
     },
-    {
-        id: 8,
-        name: "Tutorial 8",
+    {   // 9
+        id: 9,
+        name: "level 9",
         size: 6,
         areas: [
             {
@@ -260,9 +293,9 @@ const PUZZLE_MAPS = [
             [0, 0, 0, 0, 0, 0]
         ]
     },
-    {
-        id: 9,
-        name: "Tutorial 9",
+    {   // 10
+        id: 10,
+        name: "level 10",
         size: 6,
         areas: [
             {
@@ -299,9 +332,9 @@ const PUZZLE_MAPS = [
             [0, 0, 0, 0, 0, 0]
         ]
     },
-    {
-        id: 10,
-        name: "Tutorial 10",
+    {   // 11
+        id: 11,
+        name: "level 11",
         size: 6,
         areas: [
             {
@@ -334,9 +367,9 @@ const PUZZLE_MAPS = [
             [0, 0, 0, 0, 0, 0]
         ]
     },
-    {
-        id: 11,
-        name: "Level 11",
+    {   // 12
+        id: 12,
+        name: "Level 12",
         size: 6,
         areas: [
             {
@@ -392,9 +425,9 @@ const PUZZLE_MAPS = [
             [0, 0, 0, 0, 0, 0]
         ]
     },
-    {
-        id: 12,
-        name: "Level 12",
+    {   // 13
+        id: 13,
+        name: "Level 13",
         size: 6,
         areas: [
             {
@@ -423,9 +456,9 @@ const PUZZLE_MAPS = [
             [0, 0, 0, 0, 0, 0]
         ]
     },
-    {
-        id: 13,
-        name: "Level 13",
+    {   // 14
+        id: 14,
+        name: "Level 14",
         size: 6,
         areas: [
             {
@@ -454,33 +487,5 @@ const PUZZLE_MAPS = [
 
 
 /*
-{
-    id: 4,
-    name: "Tutorial 4",
-    size: 4,
-    areas: [
-        {
-            cells: [[0,0], [0,1]],
-            required: 1
-        },
-        {
-            cells: [[3,2], [3,3]],
-            required: 1
-        },
-        {
-            cells: [[2,0], [3,0], [2,1], [2,2]],
-            required: 4
-        },
-        {
-            cells: [[0,3], [1,3], [1,2], [1,1]],
-            required: 4
-        }
-    ],
-    initialState: [
-        [0, 0, 0, 0],
-        [2, 0, 0, 0],
-        [0, 0, 0, 2],
-        [0, 0, 0, 0]
-    ]
-}
+    
 */
